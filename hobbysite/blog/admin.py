44f3a9c51d0b8366baaf4801be3ab4c5d76c69e2
calm-chaos <jianna.corpuz@student.ateneo.edu>
2024-03-16 12:56:39 +0800

    created admin panels for article and article category

--- hobbysite/blog/admin.py
+++ hobbysite/blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import ArticleCategory, Article
 
-# Register your models here.
+
+class ArticleCategoryAdmin(admin.ModelAdmin):
+    model = Article
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    model = Article
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(ArticleCategory, ArticleCategoryAdmin)
