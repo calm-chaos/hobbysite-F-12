5012e2d81cddc771f4fbd5d8bd7d57e9502f52b5
Patricia Tan <157680599+patriciaTan@users.noreply.github.com>
2024-03-17 15:35:08 +0800

    created url paths in hobbysite and merchstore

--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
+from .views import MerchStoreListView, MerchStoreDetailView
+
 urlpatterns = [
-    
+    path('items', MerchStoreListView.as_view(), name='merchstore-list'),
+    path('item/<int:pk>', MerchStoreDetailView.as_view(), name='merchstore-detail'),
 ]
 
 app_name = 'merchstore'
