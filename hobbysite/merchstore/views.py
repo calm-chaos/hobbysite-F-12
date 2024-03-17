f778fe3c2e631a926d3da7b840ace62d8080089f
Patricia Tan <157680599+patriciaTan@users.noreply.github.com>
2024-03-17 15:21:28 +0800

    made views and templates for list and detail view

--- hobbysite/merchstore/views.py
+++ hobbysite/merchstore/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.list import DetailView
 
+from .models import Product
 # Create your views here.
+
+class MerchStoreListView(ListView):
+    model = Product
+    template_name = 'merchstore_list.html'
+
+
+class MerchStoreDetailView(DetailView):
+    model = Product
+    template_name = 'merchstore_detail.html'
