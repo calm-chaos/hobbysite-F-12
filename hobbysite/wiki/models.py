f9a9d96ac1a81e862fbeb879e822d0a8f1fb04b9
Zoe Ongkiko <146398841+ZoeOngkiko@users.noreply.github.com>
2024-03-20 06:45:56 +0800

    added ArticleCategory and Article  models.

--- hobbysite/wiki/models.py
+++ hobbysite/wiki/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class ArticleCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name_plural = "Article Categories"
+
+    def __str__(self):
+        return "{}".format(self.name)
+
+    def get_absolute_url(self):
+        return reverse("wiki:articlecategory", args=[str(self.pk)])
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        ArticleCategory,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="articles",
+    )
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True, null=False)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return "{}".format(self.title)
+
+    def get_absolute_url(self):
+        return reverse("wiki:article", args=[str(self.pk)])
