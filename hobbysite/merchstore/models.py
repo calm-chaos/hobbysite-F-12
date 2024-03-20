b1d38e08848da9fe27b4b2e76c33fd7f33eb485a
Patricia Tan <157680599+patriciaTan@users.noreply.github.com>
2024-03-20 17:45:26 +0800

    Removed comments to abide by PEP8

--- hobbysite/merchstore/models.py
+++ hobbysite/merchstore/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.urls import reverse
 
-# Create your models here.
 
 class ProductType(models.Model):
     name = models.CharField(max_length=255)
--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views import MerchStoreListView, MerchStoreDetailView
 
+
 urlpatterns = [
     path('items', MerchStoreListView.as_view(), name='products'),
     path('item/<int:pk>', MerchStoreDetailView.as_view(), name='product-detail'),
