b26d1c03ffddac4e661acb4e579784855363d235
Zoe Ongkiko <146398841+ZoeOngkiko@users.noreply.github.com>
2024-05-03 14:05:18 +0800

    created navigation bar and renamed commissions template

--- hobbysite/commissions/urls.py
+++ hobbysite/commissions/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import CommissionListView, CommissionDetailView
+
+from .views import CommissionDetailView, CommissionListView
 
 urlpatterns = [
-path('commissions/list', CommissionListView.as_view(), name='list'),
-path('commissions/<int:pk>', CommissionDetailView.as_view(), name='commission_detail')
-
+    path("commissions/list", CommissionListView.as_view(), name="list"),
+    path(
+        "commissions/<int:pk>", CommissionDetailView.as_view(), name="commission_detail"
+    ),
 ]
 
-# This might be needed, depending on your Django version
 app_name = "commissions"
