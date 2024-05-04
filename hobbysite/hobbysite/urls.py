66b9d80401e21bcfde95f44e008568b2d54b7efe
Zoe Ongkiko <146398841+ZoeOngkiko@users.noreply.github.com>
2024-05-04 09:20:41 +0800

    added home page, modified template files for consistency, customised and debugged navigation bar

--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -25,4 +26,5 @@ urlpatterns = [
     path("", include("commissions.urls", namespace="commissions")),
     path("merchstore/", include("merchstore.urls", namespace="merchstore")),
     path("profile/", include("django.contrib.auth.urls")),
+    path("home", TemplateView.as_view(template_name="home_page.html"), name="home"),
 ]
