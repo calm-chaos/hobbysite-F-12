05cf5cab74ef4e8b119a5a2e5b539c4bda0a9a1c
Patricia Tan <157680599+patriciaTan@users.noreply.github.com>
2024-05-05 00:18:27 +0800

    Updated Product and Transaction models

--- hobbysite/merchstore/models.py
+++ hobbysite/merchstore/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-
+from user_management.models import Profile
 
 class ProductType(models.Model):
     name = models.CharField(max_length=255)
@@ -23,8 +23,25 @@ class Product(models.Model):
         on_delete=models.SET_NULL, 
         related_name='products'
     )
+    owner = models.ForeignKey(
+        Profile,
+        null=True,
+        on_delete=models.CASCADE
+    )
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField(null=True)
+
+    class ProductStatus(models.TextChoices):
+        AVAILABLE = 'Available'
+        ON_SALE = 'On Sale'
+        OUT_OF_STOCK = 'Out of Stock'
+
+    status = models.CharField(
+        max_length=100,
+        choices=ProductStatus.choices,
+        default=ProductStatus.AVAILABLE
+    )
 
     def __str__(self):
         return self.name
@@ -34,3 +51,30 @@ class Product(models.Model):
     
     class Meta: 
         ordering = ['name']
+
+
+class Transaction(models.Model): 
+    buyer = models.ForeignKey(
+        Profile, 
+        null=True,
+        on_delete=models.SET_NULL
+    )
+    product = models.ForeignKey(
+        Product, 
+        null=True,
+        on_delete=models.SET_NULL
+    )
+    amount = models.IntegerField()
+
+    class TransactionStatus(models.TextChoices):
+        ON_CART = 'On Cart'
+        TO_PAY = 'To Pay'
+        TO_SHIP = 'To Ship'
+        TO_RECEIVE = 'To Receive'
+        DELIVERED = 'Delivered'
+
+    status = models.CharField(
+        max_length=100,
+        choices=TransactionStatus.choices
+    )
+    createdOn = models.DateTimeField(auto_now_add=True, null=True)
