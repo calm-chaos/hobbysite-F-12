0b817683b4d916448525dfb97f9a38265e474549
calm-chaos <jianna.corpuz@student.ateneo.edu>
2024-05-06 18:38:51 +0800

    added to accomodate for media and redirects

--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -18,6 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -27,4 +30,7 @@ urlpatterns = [
     path("merchstore/", include("merchstore.urls", namespace="merchstore")),
     path("profile/", include("user_management.urls", namespace="user-management")),
     path("home/", TemplateView.as_view(template_name="home_page.html"), name="home"),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
