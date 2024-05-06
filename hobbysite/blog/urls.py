dd69076ed88797f756593a8bc2ad221dd5a76d91
calm-chaos <jianna.corpuz@student.ateneo.edu>
2024-05-06 18:59:58 +0800

    added path to the gallery

--- hobbysite/blog/urls.py
+++ hobbysite/blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("article/<int:pk>/", blog_detail, name="article-detail"),
     path("article/add/", blog_create, name="create"),
     path("article/<int:pk>/edit", blog_update, name="edit"),
+    path("gallery", blog_gallery, name="gallery"),
 ]
 
 app_name = "blog"
