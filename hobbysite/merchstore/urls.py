4b14a4f40c7f582f6f54b26c1c994220a258489e
Patricia Tan <157680599+patriciaTan@users.noreply.github.com>
2024-05-07 15:51:36 +0800

    Updated urls.py

--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     path('items', product_list, name='products'),
     path('item/<int:pk>', product_detail, name='product_detail'),
     path('item/add', product_create, name='product_create'),
-    # path('item/<int:pk>/edit', product_update ),
-    # path('cart', product_cart, ),
-    # path('transactions', transaction_list),
+    path('item/<int:pk>/edit', product_update, name='product_update'),
+    path('cart', product_cart, name='product_cart'),
+    path('transactions', transaction_list, name='transaction_list'),
 ]
 
 app_name = 'merchstore'
