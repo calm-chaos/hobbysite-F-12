830f29a0664db0d08951250e5fee64e75a73c6bb
AvielleGregorio <sophia.gregorio@student.ateneo.edu>
2024-05-07 23:25:37 +0800

    Updated the admin.py to make the new inlines

--- hobbysite/commissions/admin.py
+++ hobbysite/commissions/admin.py
@@ -1,12 +1,25 @@
 from django.contrib import admin
 
-from .models import Commission
+from .models import Commission, Job, JobApplication
 
+class JobInlineAdmin(admin.TabularInline):
+    model = Job
+
+class JobApplicationInlineAdmin(admin.TabularInline):
+    model = JobApplication
+
+# class CommentInlineAdmin(admin.TabularInline):
+#     model = JobApplication
 
 class CommissionAdmin(admin.ModelAdmin):
     model = Commission
+    inlines = [JobInlineAdmin,]
 
+class JobApplicationAdmin(admin.ModelAdmin):
+    model = Job
+    inlines = [JobApplicationInlineAdmin,]
 
 admin.site.register(Commission, CommissionAdmin)
+admin.site.register(Job, JobApplicationAdmin)
 
 # Register your models here.
